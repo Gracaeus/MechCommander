d10d72e9542142e19b71365ae54399c333bd71f0
Gracaeus <tictacasian@gmail.com>
2018-10-08 15:58:24 +0100

    Hello First stuff

---
+++ Game mechanics test game/Assets/Script/bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour {
+    public float speed = 5.0f;
+    public float destroyTime = 0.07f;
+
+    
+    void Start()
+    {
+        Invoke("Die", destroyTime);
+
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+}
+
---
+++ Game mechanics test game/Assets/Script/fireBullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireBullet : MonoBehaviour {
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float fireTime = 0.1f;
+    private bool isFiring = false;
+
+    // Use this for initialization
+   void Fire()
+    {
+        isFiring = true;
+        Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+
+        Invoke("SetFiring", fireTime);
+    }
+	
+    void SetFiring()
+    {
+        isFiring = false;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0))
+        {
+            if (isFiring == false)
+            {
+                Fire();
+            }
+            
+        }
+	}
+}
