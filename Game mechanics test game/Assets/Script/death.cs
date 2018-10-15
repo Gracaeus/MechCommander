39f390923f842ed1b6680e469618e2c32e95628a
Gracaeus <tictacasian@gmail.com>
2018-10-15 15:51:39 +0100

    Shield Update
    Added the activation of the shield

---
+++ Game mechanics test game/Assets/Script/death.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class death : MonoBehaviour {
+    public GameObject shield;
+    public shieldBehaviour shieldHealth;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.CompareTag("bullet"))
+        {
+            if (shieldHealth.shieldHP >= 1)
+            {
+                shield.SetActive(true);
+            }
+            else
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+            }
+           
+        }
+    }
+}
---
+++ Game mechanics test game/Assets/Script/shieldBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shieldBehaviour : MonoBehaviour {
+
+    public int shieldHP = 100;
+
+	
+	private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.CompareTag("bullet"))
+        {
+            shieldHP -= 10;
+            System.Console.WriteLine(shieldHP);
+            if (shieldHP<=0)
+            {
+                Destroy(gameObject);
+            }
+        }
+       
+    }
+}
