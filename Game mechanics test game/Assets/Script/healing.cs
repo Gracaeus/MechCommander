3ce00359ee5b26c0091f44f7fe69eb97062915b2
Kenji Honculada <kenji@Space-Cowboy.local>
2018-11-26 16:12:29 +0000

    Revert "Turn based fixed"
    This reverts commit 9c578a98b7c6515dc996e1ef52eb04393c4a0bee.

--- Game mechanics test game/Assets/Script/healing.cs
+++ Game mechanics test game/Assets/Script/healing.cs
@@ -9,11 +9,18 @@ public class healing : MonoBehaviour {
 	public Transform healingSpawn;
 	public float destroyTime = 1f;
 
+<<<<<<< HEAD
 
    
+=======
+>>>>>>> parent of 9c578a9... Turn based fixed
 	public void SpawnHealth()
 	{
 		Instantiate(healingPrefab, healingSpawn.position, healingSpawn.rotation);
+		Invoke("Die", destroyTime);
 	}
-	
+	void Die()
+	{
+		Destroy(gameObject);
+	}
 }
